7770761d2fdb0d160e977e75ca1c25fa19429be5
DrashtiVekariya0019 <127108475+DrashtiVekariya0019@users.noreply.github.com>
2025-11-11 15:13:00 +0530

    done

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cd9b05c3c9186402e0f35c6aba5343e3
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "bcfee87ac5a46b54c4fe8635307bcb94",
+"flutter_bootstrap.js": "55524406049e7bac05a10fa35ab06175",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
